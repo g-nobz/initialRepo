a36bef12f55de8e52476cb21d01507c57a17e912
g-nobz <gregorynobleza@ufl.edu>
2022-03-14 23:16:19 -0400

    adds two numbers, 1 and 40

---
+++ AddProgram.java
@@ -0,0 +1,13 @@
+public class AddProgram {
+
+    public static void main(String[] args){
+
+        System.out.println("1st Number: 1");
+        int firstNum = 1;
+        System.out.println("2nd Number: 40");
+        int secNum = 40;
+        int sum = firstNum + secNum;
+        System.out.println("1 + 40 = " +  sum);
+
+    }
+}
